0df33dabce04d0bae2174f0949339156b3a5a66d
ducddm <47740175+ducddm@users.noreply.github.com>
2019-12-06 10:24:57 +0700

    Practical Work 16

--- src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -26,6 +26,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.tabs.TabLayout;
 
 import org.w3c.dom.Text;
@@ -106,8 +111,34 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
         t.start();
-    }
 
+
+    }
+    private void request(){
+        // once, should be performed once per app instance
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        // a listener (kinda similar to onPostExecute())
+        Response.Listener<Bitmap> listener =
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        ImageView iv = (ImageView) findViewById(R.id.logo);
+                        iv.setImageBitmap(response);
+                    }
+                };
+        // a simple request to the required image
+        ImageRequest imageRequest = new ImageRequest(
+                "https://ictlab.usth.edu.vn/wp-content/uploads/logos/usth.png",
+                listener, 0, 0, ImageView.ScaleType.FIT_CENTER,
+                Bitmap.Config.ARGB_8888, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(WeatherActivity.this, "Response is: " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+        // go!
+        queue. add(imageRequest);
+    }
     private class AsyncTaskRunner extends AsyncTask<URL, Integer, Bitmap> {
         Bitmap bitmap;
         ProgressDialog progressDialog;
@@ -215,7 +246,7 @@ public class WeatherActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.refresh:
-                new AsyncTaskRunner().execute(url);
+                request();
                 return true;
             case R.id.settings:
                 Intent intent = new Intent(this, PrefActivity.class);
